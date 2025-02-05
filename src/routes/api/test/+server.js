8cc19f3a69ae6de64ab917157cf5742570802ff9
Savas Vedova <savas@stormkit.io>
2025-02-05 11:13:28 +0100

    add server side dependency

--- src/routes/api/test/+server.js
+++ src/routes/api/test/+server.js
@@ -1,5 +1,7 @@
 import { json } from "@sveltejs/kit";
+import Database from "better-sqlite3";
 
 export function GET({ locals, url }) {
+  console.log(Database);
   return json({ hello: "world" });
 }
